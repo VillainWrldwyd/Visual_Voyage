62efdbad7d0b1d8e10f39d8f861d2b4affef7136
VillainWrldwyd <122698047+VillainWrldwyd@users.noreply.github.com>
2026-05-14 20:05:38 +0200

    Update scriptTT.js

--- scriptTT.js
+++ scriptTT.js
@@ -493,7 +493,7 @@ initDashboard();
 // Trade Tracker app//
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register("/service-worker.js")
+    .register("APPjson/service-worker.js")
     .then((reg) => console.log("Service Worker registered:", reg))
     .catch((err) => console.log("Service Worker failed:", err));
 }
